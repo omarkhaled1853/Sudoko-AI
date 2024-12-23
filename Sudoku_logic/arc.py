35cc5c9d0a5a31f346c283223b84edaca8a5a251
Omer Khaled <Omer Khaled@DESKTOP-IL45J4J>
2024-12-23 20:07:06 +0200

    Merge sudoko_logic branch

---
+++ Sudoku_logic/arc.py
@@ -0,0 +1,260 @@
+import copy
+from collections import deque
+import random
+
+class SudokuSolver:
+    def __init__(self, board):
+        self.board = board
+
+        self.variables = self._create_variables()
+        self.neighbors = self._create_neighbors(self.variables)
+        self.domain = self._create_domain(self.board)
+
+        self.all_solutions = []
+        self.res = 0
+
+
+    def _create_variables(self):
+        variables = []
+        for i in range(9):
+            for j in range(9):
+                variables.append((i, j))
+        return variables
+
+    def _get_row_neighbors(self, var):
+        neighbors = []
+        for i in range(9):
+            if i != var[1]:
+                neighbors.append((var[0], i))
+        return neighbors
+
+    def _get_col_neighbors(self, var):
+        neighbors = []
+        for i in range(9):
+            if i != var[0]:
+                neighbors.append((i, var[1]))
+        return neighbors
+
+    def _get_box_neighbors(self, var):
+        row = var[0] // 3 * 3
+        col = var[1] // 3 * 3
+        neighbors = []
+        for i in range(row, row + 3):
+            for j in range(col, col + 3):
+                if (i, j) != var:
+                    neighbors.append((i, j))
+        return neighbors
+
+    def _get_all_neighbors(self, x):
+        row_neighbors = self._get_row_neighbors(x)
+        col_neighbors = self._get_col_neighbors(x)
+        box_neighbors = self._get_box_neighbors(x)
+        return row_neighbors + col_neighbors + box_neighbors
+
+    def _create_neighbors(self, variables):
+        neighbors = {}
+        for variable in variables:
+            neighbors[variable] = self._get_all_neighbors(variable)
+        return neighbors
+
+    def _delete_constraint_values(self, values: [], neighbors: []):
+        for neighbor in neighbors:
+            if self.board[neighbor[0]][neighbor[1]] != '0':
+                if self.board[neighbor[0]][neighbor[1]] in values:
+                    values.remove(self.board[neighbor[0]][neighbor[1]])
+
+    def _get_valid_values(self, variable):
+        values = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+        row_neighbors = self._get_row_neighbors(variable)
+        self._delete_constraint_values(values, row_neighbors)
+
+        col_neighbors = self._get_col_neighbors(variable)
+        self._delete_constraint_values(values, col_neighbors)
+
+        box_neighbors = self._get_box_neighbors(variable)
+        self._delete_constraint_values(values, box_neighbors)
+
+        return values
+
+    def _create_domain(self, board):
+        domain = {}
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == '0':
+                    domain[(i, j)] = self._get_valid_values((i, j))
+                else:
+                    domain[(i, j)] = [board[i][j]]
+        return domain
+
+    def _consistent(self, val, xj):
+        for jVal in self.domain[xj]:
+            if jVal != val:
+                return True
+        return False
+
+    def _select_unassigned_variable(self):
+        variable = None
+        for var in self.variables:
+            if self.board[var[0]][var[1]] == '0':
+                if variable is None or len(self.domain[var]) < len(self.domain[variable]):
+                    variable = var
+        return variable
+
+    def _number_of_role_out(self, variable, value):
+        var_neighbors = self._get_all_neighbors(variable)
+        role_out = 0
+        for neighbor in var_neighbors:
+            if value in self.domain[neighbor]:
+                role_out += 1
+        return role_out
+
+    def _order_domain_values(self, variable):
+        valid_values = self._get_valid_values(variable)
+        order_domain = sorted(valid_values, key=lambda value: self._number_of_role_out(variable, value))
+        return order_domain
+
+    def _valid_move(self, var, value):
+        variable_neighbors = self._get_all_neighbors(var)
+        for neighbor in variable_neighbors:
+            if self.board[neighbor[0]][neighbor[1]] == value:
+                return False
+        return True
+
+    def _game_solved(self):
+        for i in range(9):
+            for j in range(9):
+                if self.board[i][j] == '0':
+                    return False
+        return True
+
+    def ac_3(self):
+        queue = deque()
+        for var, neighbors_list in self.neighbors.items():
+            for neighbor in neighbors_list:
+                queue.append((var, neighbor))
+
+        while queue:
+            top = queue.popleft()
+            if self._revise(top[0], top[1]):
+                if len(self.domain[top[0]]) == 0:
+                    return False
+                all_neighbors = self._get_all_neighbors(top[0])
+                for neighbor in all_neighbors:
+                    if neighbor == top[1]:
+                        continue
+                    queue.append((neighbor, top[0]))
+        return True
+
+    def _revise(self, xi, xj):
+        revised = False
+        values_to_remove = []
+        for val in self.domain[xi]:
+            if not self._consistent(val, xj):
+                values_to_remove.append(val)
+                revised = True
+        for val in values_to_remove:
+            self.domain[xi].remove(val)
+        return revised
+
+
+    def _backtrack(self):
+        if self._game_solved():
+            self.all_solutions.append(copy.deepcopy(self.board))
+            self.res += 1
+            return
+
+        var = self._select_unassigned_variable()
+        order_values = self._order_domain_values(var)
+
+        for value in order_values:
+            self.board[var[0]][var[1]] = value
+            if self.ac_3():
+                self._backtrack()
+
+            self.board[var[0]][var[1]] = '0'
+
+
+    def solve(self):
+        self.ac_3()
+        self._backtrack()
+
+        if len(self.all_solutions) > 0:
+            return self.all_solutions[0]
+
+    def get_solutions(self):
+        return self.all_solutions
+
+
+
+
+#
+# int_board = random_sudoku.get_board()
+# board = [[str(i) for i in row] for row in int_board]
+#
+# sudoku_solver = SudokuSolver()
+# sudoku_solver.solve(board)
+#
+# solutions = sudoku_solver.get_solutions()
+# print(len(solutions))
+#
+# while len(solutions) > 1:
+#     # Find conflicting positions across all solutions
+#     conflicting_cells = find_conflicting_cells(solutions)
+#
+#     # Fill the conflicting cells with any valid value (choose one from the conflicting solutions)
+#     for (i, j) in conflicting_cells:
+#         possible_values = [solution[i][j] for solution in solutions]
+#         chosen_value = random.choice(possible_values)
+#         board[i][j] = chosen_value
+#
+#     # Clear solutions and solve again
+#     sudoku_solve = SudokuSolver()  # Create a new solver with updated board
+#     sudoku_solve.solve(board)
+#     solutions = sudoku_solve.get_solutions()
+#
+
+
+
+
+# random_sudoku.ensure_unique_solution(sudoku_solver)
+#
+# int_board = random_sudoku.get_board()
+# board = [[str(i) for i in row] for row in int_board]
+#
+# for i in range(9):
+#     for j in range(9):
+#         print(board[i][j] + " ", end="")
+#     print()
+#
+
+
+# board = [
+#     ['6','0','0','0','0','0','8','0','0'],
+#     ['0','0','2','0','9','0','0','4','0'],
+#     ['0','0','7','0','8','4','1','0','0'],
+#     ['0','0','0','9','0','0','0','6','0'],
+#     ['5','8','0','0','0','3','0','2','9'],
+#     ['0','0','9','4','0','0','0','0','0'],
+#     ['9','4','0','0','0','0','0','0','7'],
+#     ['0','0','0','6','0','2','0','8','0'],
+#     ['0','0','0','0','4','0','6','0','3']
+# ]
+#
+#
+# sod_solver = SudokuSolver(board)
+# result = sod_solver.solve()
+# print(sod_solver.res)
+# for solution in sod_solver.all_solutions:
+#     for i in range(9):
+#         for j in range(9):
+#             print(solution[i][j] + " ", end="")
+#         print()
+#
+#     print()
+#
+#
+# print(len(sod_solver.all_solutions))
+# for i in range(9):
+#     for j in range(9):
+#         print(result[i][j] + " ", end="")
+#     print()
---
+++ Sudoku_logic/random_sudoku_board.py
@@ -0,0 +1,161 @@
+import random
+import math
+from Lib.arc import SudokuSolver
+
+class random_sudoku_board:
+    def __init__(self, N, K):
+        self.N = N
+        self.K = K
+        self.SRN = int(math.sqrt(N))
+
+        self.mat = [[0 for _ in range(N)] for _ in range(N)]
+
+    def fill_board(self):
+        self.fill_diagnoal()
+
+        self.fill_undiagonal(0, self.SRN)
+
+        self.remove_k_digit_random()
+
+
+    def fill_diagnoal(self):
+        for i in range(0, self.N, self.SRN):
+            self.fill_box(i, i)
+
+    def fill_box(self, start_row, start_col):
+        for i in range(self.SRN):
+            for j in range(self.SRN):
+                while True:
+                    num = self.random_generator()
+                    if self.valid_play_in_box(start_row, start_col, num):
+                        self.mat[start_row + i][start_col + j] = num
+                        break
+
+    def random_generator(self):
+        return math.floor(random.random() * self.N + 1)
+
+    def valid_play_in_box(self, start_row, start_col, num):
+        for i in range(self.SRN):
+            for j in range(self.SRN):
+                if self.mat[start_row + i][start_col + j] == num:
+                    return False
+
+        return True
+
+    def valid_play_in_row(self, row, num):
+        for col in range(self.N):
+            if self.mat[row][col] == num:
+                return False
+
+        return True
+
+    def valid_play_in_col(self, col, num):
+        for row in range(self.N):
+            if self.mat[row][col] == num:
+                return False
+
+        return True
+
+    def valid_play(self, row, col, num):
+        return (self.valid_play_in_row(row, num) and
+                self.valid_play_in_col(col, num) and
+                self.valid_play_in_box(row - row % self.SRN, col - col % self.SRN, num))
+
+    def fill_undiagonal(self, i, j):
+        if i >= self.N and j >= self.N:
+            return True
+
+        if j >= self.N:
+            i += 1
+            j = 0
+            if i >= self.N:
+                return True
+
+        if self.mat[i][j] != 0:
+            return self.fill_undiagonal(i, j + 1)
+
+        for num in range(1, self.N + 1):
+            if self.valid_play(i, j, num):
+                self.mat[i][j] = num
+                if self.fill_undiagonal(i, j + 1):
+                    return True
+
+                self.mat[i][j] = 0
+
+        return False
+
+    def remove_k_digit_random(self):
+
+        count = self.K
+        while count != 0:
+            i = self.random_generator() - 1
+            j = self.random_generator() - 1
+
+            if self.mat[i][j] != 0:
+                self.mat[i][j] = 0
+                count -= 1
+
+
+    def get_board(self):
+        """ Return the generated Sudoku board. """
+        return self.mat
+
+    def find_conflicting_cells(self, solutions):
+        conflicting_cells = []
+        for i in range(self.N):
+            for j in range(self.N):
+                values = [solution[i][j] for solution in solutions]
+                if len(set(values)) > 1:  # Multiple values in different solutions
+                    conflicting_cells.append((i, j))
+                    return conflicting_cells
+        return conflicting_cells
+
+    def get_board_with_unique_solutino(self):
+        self.fill_diagnoal()
+
+        self.fill_undiagonal(0, self.SRN)
+
+        self.remove_k_digit_random()
+
+        board = [[str(i) for i in row] for row in self.mat]
+
+        sudoku_solver = SudokuSolver(board)
+        sudoku_solver.solve()
+
+        solutions = sudoku_solver.get_solutions()
+
+        while len(solutions) > 1:
+            # Find conflicting positions across all solutions
+            conflicting_cells = self.find_conflicting_cells(solutions)
+
+            if len(conflicting_cells) == 0:
+                break
+
+            # Fill the conflicting cells with any valid value (choose one from the conflicting solutions)
+            for (i, j) in conflicting_cells:
+                possible_values = [solution[i][j] for solution in solutions]
+                chosen_value = random.choice(possible_values)
+                board[i][j] = chosen_value
+
+            # Clear solutions and solve again
+            sudoku_solve = SudokuSolver(board)  # Create a new solver with updated board
+            sudoku_solve.solve()
+            solutions = sudoku_solve.get_solutions()
+
+        return board
+
+
+random_sudoku = random_sudoku_board(9, 30)
+board = random_sudoku.get_board_with_unique_solutino()
+
+k = 0
+for i in range(9):
+    for j in range(9):
+        if board[i][j] == '0':
+            k += 1
+        print(board[i][j] + " ", end="")
+    print()
+
+
+print(k)
+
