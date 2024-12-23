05c88b15544a17e88509c40790864b397238a4a5
Omer Khaled <Omer Khaled@DESKTOP-IL45J4J>
2024-12-23 14:29:49 +0200

    Adding sudoku into mode one and make some modifications

--- Sudoku_GUI/app.py
+++ Sudoku_GUI/app.py
@@ -9,15 +9,9 @@ class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Sudoku Game")
-
-        # Set the initial size of the window
-        self.geometry("600x500")
         
         # Allow the window to be resizable
         self.resizable(True, True)
-
-        # Center the window on the screen
-        self.center_window(600, 500)
 
         # Create a container frame for all pages
         container = tk.Frame(self)
@@ -32,15 +26,6 @@ class App(tk.Tk):
 
         # Show the menu page initially
         self.menu_page.show()
-
-
-    def center_window(self, width, height):
-        """Center the application window on the screen."""
-        screen_width = self.winfo_screenwidth()
-        screen_height = self.winfo_screenheight()
-        x = (screen_width - width) // 2
-        y = (screen_height - height) // 2
-        self.geometry(f"{width}x{height}+{x}+{y}")
     
     def show_menu(self):
         """Show menu page and hide all other pages"""
